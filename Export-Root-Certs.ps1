b054f37fd33cc921665c2092db7822f09ed81487
dhabben <dhabben@gmail.com>
2015-02-20 09:25:40 -0600

    Initial commit

---
+++ Export-Root-Certs.ps1
@@ -0,0 +1,27 @@
+﻿<#
+ Thanks to zakird for a starting point with his code:
+ https://gist.github.com/zakird/a8582ced2f50cfe1c702
+
+ Example:
+  Export-Root-Certs.ps1
+  Export-Root-Certs.ps1 > Current-Certs.txt
+
+ Use the output to compare to a clean install of Windows
+
+#>
+
+$dir = "C:\Export\"
+$type = [System.Security.Cryptography.X509Certificates.X509ContentType]::Cert
+
+
+Get-ChildItem -Path cert:\LocalMachine\AuthRoot | ForEach-Object {
+    Write-Output "======================================"
+    Write-Output $_.GetCertHashString()
+    Write-Output $_.FriendlyName
+    Write-Output $_.GetIssuerName()
+    Write-Output $_.GetEffectiveDateString()
+    Write-Output $_.GetExpirationDateString()
+    
+    #$cerHash = $_.GetCertHashString()
+    #[System.IO.File]::WriteAllBytes("$dir$cerHash.der", $_.export($type))
+}
